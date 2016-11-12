66988ac19469c97c13dc9f741372862f447b08b3
Martin Laberge <mlsoftlaberge@gmail.com>
2016-11-12 01:17:43 -0500

    Version 4.0 of the Famous Trycorder By Martin Laberge.

---
+++ initgit4.sh
@@ -0,0 +1,6 @@
+echo "# Trycorder4" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/mlsoft/Trycorder4.git
+git push -u origin master
